1ef12baceae1139b0aa017283a09c2ee6bb0a16a
mmb1214 <78535223+mmb1214@users.noreply.github.com>
2022-05-03 19:03:26 -0700

    Finalized

--- Final/Final.ino
+++ Final/Final.ino
@@ -5,7 +5,7 @@ int sensorValue;
 int sensorLow = 1023;
 int sensorHigh = 0;
 
-const int piezo = 9;
+const int piezo = A1;
 const int switchPin = 2;
 const int yellowLed = 3;
 const int greenLed = 4;
@@ -30,50 +30,91 @@ void setup() {
   pinMode(switchPin, INPUT);
 
   Serial.begin(9600); // Initialize serial communication with computer 
-  digitalWrite(redLed, HIGH); // Turn on the green led
+  digitalWrite(yellowLed, HIGH); // Turn on the green led
   myServo.write(0); // Move the servo to the locked position 
-  Serial.println("Blinds are open"); // right-close, left-open
+  Serial.println("Blinds are CLOSED"); // right-close, left-open
 
   sensorValue = analogRead(A0); 
 }
 
 void loop() {
+  // Read the value of vibration from the piezo and store it in knockVal
+  knockVal = analogRead(piezo);
+  switchVal = digitalRead(switchPin);
+
+  
   sensorValue = analogRead(A0);
   potVal = analogRead(sensorValue);
-  Serial.println("Light value: ");
+  Serial.print("Light value: ");
   Serial.println(potVal);
 
   // If it is bright outside 
   if(potVal > 600){
     // Open the blinds 
     angle = 180;
-    Serial.println("Angle: ");
+    Serial.print("Angle: ");
     Serial.println(angle);
     myServo.write(angle);
     delay(1000);
+
+    // Turn on the green Led
+    digitalWrite(greenLed, HIGH);
+    digitalWrite(redLed, LOW);
+    digitalWrite(yellowLed, LOW);
+
+    // Output statement
+    Serial.println("Blinds are OPEN");
+
+    Serial.print("Knock value: ");
+    Serial.println(knockVal);
+    delay(100);
+  
+    if(knockVal >= 50){
+      while(switchVal != HIGH){
+        Serial.println("!!!!!!!!!!INTRUDER ALERT!!!!!!!!!!");
+        myServo.write(0);
+        digitalWrite(redLed, HIGH);
+        delay(50);
+        digitalWrite(redLed, LOW);
+        delay(50);
+  
+        switchVal = digitalRead(switchPin);
+      }
+    }
   }
+
+  // If it is dim outside
   else{
+    // Close the blinds
     angle = 0;
-    Serial.println(", angle ");
+    Serial.print("Angle ");
     Serial.println(angle);
     myServo.write(angle);
     delay(1000);
+
+    // Turn on the yellow Led
+    digitalWrite(yellowLed, HIGH);
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, LOW);
+
+    // Output statement
+    Serial.println("Blinds are CLOSED");
+    
+    Serial.print("Knock value: ");
+    Serial.println(knockVal);
+    delay(100);
+    
+    if(knockVal >= 50){
+      while(switchVal != HIGH){
+        Serial.println("!!!!!!!!!!INTRUDER ALERT!!!!!!!!!!");
+        myServo.write(0);
+        digitalWrite(redLed, HIGH);
+        delay(50);
+        digitalWrite(redLed, LOW);
+        delay(50);
+  
+        switchVal = digitalRead(switchPin);
+      }
+    }
   }
-  
-//   sensorValue = analogRead(A0);
-//   potVal = analogRead(sensorValue);
-//   Serial.println("potVal: ");
-//   Serial.println(potVal);
-//   delay(1000);
-//   
-//   // As long as the switch has been pressed
-//   while(switchVal == HIGH){
-//    // If the 
-//    if(potVal >= 400){
-//      angle = 90;
-//      myServo.write(angle);
-//      Serial.println("Angle moved");
-//      delay(1000);
-//    } 
-  
 }
